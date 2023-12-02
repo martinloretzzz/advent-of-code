9cb437fd047171e6b6a8238f490678cc04ae9d33
0x4d - Martin Loretz <20306567+martinloretzzz@users.noreply.github.com>
2023-12-02 16:57:25 +0700

    Solve puzzle day 2 part 2

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -14,6 +14,7 @@ fn main() {
     let input = std::fs::read_to_string(&file).unwrap();
 
     let mut sum = 0;
+    let mut sum_min = 0;
 
     for line in input.lines() {
         let mut split = line.split(':');
@@ -26,6 +27,10 @@ fn main() {
         let game_id_num = game_id_parts.next().unwrap().parse::<i32>().unwrap();
 
         let mut invalid = false;
+
+        let mut red_min = 0;
+        let mut green_min = 0;
+        let mut blue_min = 0;
 
         for take in game_data.split(";") {
             let mut red_count = 0;
@@ -50,10 +55,17 @@ fn main() {
             if red_count > 12 || green_count > 13 || blue_count > 14 {
                 invalid = true;
             }
+            red_min = red_min.max(red_count);
+            green_min = green_min.max(green_count);
+            blue_min = blue_min.max(blue_count);
         }
         if !invalid {
             sum += game_id_num;
         }
+        let min_power = red_min * green_min * blue_min;
+        sum_min += min_power;
     }
-    println!("{sum}");
+
+    println!("Sum of Ids: {sum}");
+    println!("Sum of the powers of the min cubes: {sum_min}");
 }
