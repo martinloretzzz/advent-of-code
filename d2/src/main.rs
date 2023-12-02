5f8604267fcbd2fe951a7b39ff586fdbe139c4bd
0x4d - Martin Loretz <20306567+martinloretzzz@users.noreply.github.com>
2023-12-02 17:20:44 +0700

    Refactor puzzle day 2

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -5,7 +5,6 @@ use std::path::PathBuf;
 #[command(arg_required_else_help = true)]
 struct Cli {
     file: PathBuf,
-    part: i32,
 }
 
 fn main() {
@@ -14,17 +13,14 @@ fn main() {
     let input = std::fs::read_to_string(&file).unwrap();
 
     let mut sum = 0;
-    let mut sum_min = 0;
+    let mut sum_power_cube_min = 0;
 
     for line in input.lines() {
-        let mut split = line.split(':');
+        let line_parts = line.split(':').collect::<Vec<&str>>();
+        let [game_id, game_data] = <[&str; 2]>::try_from(line_parts).ok().unwrap();
 
-        let game_id = split.next().unwrap();
-        let game_data = split.next().unwrap();
-
-        let mut game_id_parts = game_id.split(" ");
-        game_id_parts.next();
-        let game_id_num = game_id_parts.next().unwrap().parse::<i32>().unwrap();
+        let game_id_parts = game_id.split(" ").collect::<Vec<&str>>();
+        let game_id_num = game_id_parts[1].parse::<i32>().unwrap();
 
         let mut invalid = false;
 
@@ -38,9 +34,9 @@ fn main() {
             let mut blue_count = 0;
             for cube in take.split(",") {
                 let cube = cube.trim();
-                let mut cube_parts = cube.split(" ");
-                let count = cube_parts.next().unwrap().parse::<i32>().unwrap();
-                let color = cube_parts.next().unwrap();
+                let cube_parts = cube.split(" ").collect::<Vec<&str>>();
+                let count = cube_parts[0].parse::<i32>().unwrap();
+                let color = cube_parts[1];
 
                 if color == "red" {
                     red_count += count;
@@ -63,9 +59,9 @@ fn main() {
             sum += game_id_num;
         }
         let min_power = red_min * green_min * blue_min;
-        sum_min += min_power;
+        sum_power_cube_min += min_power;
     }
 
     println!("Sum of Ids: {sum}");
-    println!("Sum of the powers of the min cubes: {sum_min}");
+    println!("Sum of the powers of the min cubes: {sum_power_cube_min}");
 }
