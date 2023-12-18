7b31497d3ac08fd8a16c1e79551169d40142a6ee
0x4d - Martin Loretz <20306567+martinloretzzz@users.noreply.github.com>
2023-12-18 22:56:12 +0700

    Refactor puzzle day 10

--- d10/src/main.rs
+++ d10/src/main.rs
@@ -8,41 +8,41 @@ struct Cli {
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
-struct Vec2 {
+struct Pos2 {
     x: i32,
     y: i32,
 }
 
-fn move_pos(p: Vec2, dx1: i32, dy1: i32, dx2: i32, dy2: i32) -> (Vec2, Vec2) {
-    return (
-        Vec2 {
-            x: p.x + dx1,
-            y: p.y + dy1,
-        },
-        Vec2 {
-            x: p.x + dx2,
-            y: p.y + dy2,
-        },
-    );
+impl Pos2 {
+    fn offset(&self, dx: i32, dy: i32) -> Pos2 {
+        return Pos2 {
+            x: self.x + dx,
+            y: self.y + dy,
+        };
+    }
 }
 
-fn next_pos_options(p: Vec2, pipe: &str) -> (Vec2, Vec2) {
-    return match pipe {
-        "-" => move_pos(p, -1, 0, 1, 0),
-        "|" => move_pos(p, 0, -1, 0, 1),
+#[derive(Clone, PartialEq, Eq, Debug)]
+struct PipePointer {
+    last: Pos2,
+    pos: Pos2,
+}
 
-        "L" => move_pos(p, 0, -1, 1, 0),
-        "J" => move_pos(p, 0, -1, -1, 0),
-        "7" => move_pos(p, 0, 1, -1, 0),
-        "F" => move_pos(p, 0, 1, 1, 0),
+fn next_pos(p: Pos2, last: Pos2, pipe: &str) -> Pos2 {
+    let (p1, p2) = match pipe {
+        "-" => (p.offset(-1, 0), p.offset(1, 0)),
+        "|" => (p.offset(0, -1), p.offset(0, 1)),
+
+        "L" => (p.offset(0, -1), p.offset(1, 0)),
+        "J" => (p.offset(0, -1), p.offset(-1, 0)),
+        "7" => (p.offset(0, 1), p.offset(-1, 0)),
+        "F" => (p.offset(0, 1), p.offset(1, 0)),
 
         "." => panic!(),
         "S" => panic!(),
         _ => panic!(),
     };
-}
 
-fn next_pos_from_options((p1, p2): (Vec2, Vec2), last: Vec2) -> Vec2 {
     if p1 == last {
         return p2;
     } else if p2 == last {
@@ -52,66 +52,69 @@ fn next_pos_from_options((p1, p2): (Vec2, Vec2), last: Vec2) -> Vec2 {
     }
 }
 
+fn move_alone_pipe(pipe_pointer: PipePointer, grid: &Vec<Vec<String>>) -> PipePointer {
+    let (pos, last) = (&pipe_pointer.pos, &pipe_pointer.last);
+    let pipe = &grid[pos.y as usize][pos.x as usize];
+    let next = next_pos(pos.clone(), last.clone(), pipe.as_ref());
+    println!("{pipe} n: {:?}, p: {:?} l: {:?}", next, pos, last,);
+    return PipePointer {
+        pos: next,
+        last: pos.clone(),
+    };
+}
+
+fn get_grid(input: String) -> Vec<Vec<String>> {
+    let mut grid: Vec<Vec<String>> = Vec::new();
+    let lines = input.lines();
+    for line in lines {
+        let row: Vec<String> = line
+            .split("")
+            .filter(|s| !s.is_empty())
+            .map(|s| s.to_string())
+            .collect();
+        grid.push(row);
+    }
+    return grid;
+}
+
+fn get_start_pipes(grid: &Vec<Vec<String>>) -> (PipePointer, PipePointer) {
+    for (y, row) in grid.into_iter().enumerate() {
+        for (x, pos) in row.into_iter().enumerate() {
+            if pos == "S" {
+                let (x, y) = (x as i32, y as i32);
+                return (
+                    PipePointer {
+                        pos: Pos2 { x: x + 1, y },
+                        last: Pos2 { x: x, y: y },
+                    },
+                    PipePointer {
+                        pos: Pos2 { x: x, y: y + 1 },
+                        last: Pos2 { x: x, y: y },
+                    },
+                );
+            }
+        }
+    }
+    panic!();
+}
+
 fn main() {
     let args = Cli::parse();
     let file = args.file;
     let input = std::fs::read_to_string(&file).unwrap();
 
-    let lines = input.lines();
-
-    let mut grid: Vec<Vec<&str>> = Vec::new();
-
-    let mut last_pos_1 = Vec2 { x: 0, y: 0 };
-    let mut last_pos_2 = Vec2 { x: 0, y: 0 };
-    let mut pos_1 = Vec2 { x: 0, y: 0 };
-    let mut pos_2 = Vec2 { x: 0, y: 0 };
-
     let mut steps = 1;
 
-    for (y, line) in lines.enumerate() {
-        // println!("{line}");
-        let row: Vec<&str> = line.split("").filter(|s| !s.is_empty()).collect();
-
-        for (x, pos) in row.clone().into_iter().enumerate() {
-            if pos == "S" {
-                let x = x as i32;
-                let y = y as i32;
-
-                let s = Vec2 { x: x, y: y };
-                last_pos_1 = s.clone();
-                last_pos_2 = s.clone();
-
-                pos_1 = Vec2 { x: x + 1, y };
-                pos_2 = Vec2 { x: x, y: y + 1 };
-            }
-        }
-
-        grid.push(row);
-    }
+    let grid: Vec<Vec<String>> = get_grid(input);
+    let (mut pipe_1, mut pipe_2) = get_start_pipes(&grid);
 
     loop {
-        let pipe_1 = grid[pos_1.y as usize][pos_1.x as usize];
-        let next_1 =
-            next_pos_from_options(next_pos_options(pos_1.clone(), pipe_1), last_pos_1.clone());
-        println!(
-            "1: {pipe_1} n: {:?}, p: {:?} l: {:?}",
-            next_1, pos_1, last_pos_1,
-        );
-        last_pos_1 = pos_1.clone();
-        pos_1 = next_1.clone();
-
-        let pipe_2 = grid[pos_2.y as usize][pos_2.x as usize];
-        let next_2 =
-            next_pos_from_options(next_pos_options(pos_2.clone(), pipe_2), last_pos_2.clone());
-        println!(
-            "2: {pipe_2} n: {:?}, p: {:?} l: {:?}",
-            next_2, pos_2, last_pos_2
-        );
-        last_pos_2 = pos_2.clone();
-        pos_2 = next_2.clone();
+        pipe_1 = move_alone_pipe(pipe_1, &grid);
+        pipe_2 = move_alone_pipe(pipe_2, &grid);
 
         steps += 1;
-        if pos_1 == pos_2 {
+
+        if pipe_1.pos == pipe_2.pos {
             break;
         }
     }
